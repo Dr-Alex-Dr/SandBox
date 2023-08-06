94d131c18363a9ab7b5f3a7327da6517130d47fb
Dr-Alex-Dr <mr.lopin900@gmail.com>
2023-08-06 12:50:34 +0500

    0.0.1

---
+++ build/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hello = 'Hello world';
+console.log(hello);
+console.log(hello);
+//# sourceMappingURL=main.js.map
